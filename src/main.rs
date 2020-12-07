692ac1be69a4d2152d2bae802ab9422799571e9b
mzyzc <mzyzc@czyz.xyz>
2020-12-07 21:29:43 +0000

    Implemented basic TCP server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{TcpListener, TcpStream};
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:63100")?;
+    println!("Listening on port 63100");
+
+    for stream in listener.incoming() {
+        handle_client(stream?);
+    }
+    Ok(())
+}
+
+fn handle_client(stream: TcpStream) {
+    println!("Successful connection from {}", stream.peer_addr().unwrap().to_string());
 }
